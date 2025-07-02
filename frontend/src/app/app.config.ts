07b45d6c2bdbb5663579b4ab753e4191d9ad04b2
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-07-02 22:43:55 +0100

    hunk

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,33 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
 import { provideRouter } from '@angular/router'
-
 import { routes } from './app.routes'
+import { InitService } from './services/init.service'
+import { provideHttpClient, withFetch } from '@angular/common/http'
+
+const initApp = (initService: InitService) => {
+  return () => {
+    initService.init()
+      .then((success: boolean) => {
+        if (success) {
+          console.log('initApp success', success)
+        } else {
+          console.error('initApp failure', success)
+        }
+      })
+      .catch((err: unknown) => console.error('initApp error', err))
+  }
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initApp,
+      deps: [InitService],
+      multi: true,
+    },
+  ],
 }
