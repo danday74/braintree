f703281ce254aa7b73bdfa4bf13504ac720be04f
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-07-02 23:24:13 +0100

    hunk

--- frontend/src/app/services/init.service.ts
+++ frontend/src/app/services/init.service.ts
@@ -28,7 +28,16 @@ export class InitService {
       return this.getClientToken().subscribe({
         next: (response: IClientToken) => {
           if (response.clientToken) {
-            localStorage.setItem('clientToken', response.clientToken)
+            const prevClientToken: string = localStorage.getItem('clientToken') ?? ''
+            const clientToken: string = response.clientToken
+
+            localStorage.setItem('prevClientToken', prevClientToken)
+            localStorage.setItem('clientToken', clientToken)
+
+            // const hardcoded = 'eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiJleUowZVhBaU9pSktWMVFpTENKaGJHY2lPaUpGVXpJMU5pSXNJbXRwWkNJNklqSXdNVGd3TkRJMk1UWXRjMkZ1WkdKdmVDSXNJbWx6Y3lJNkltaDBkSEJ6T2k4dllYQnBMbk5oYm1SaWIzZ3VZbkpoYVc1MGNtVmxaMkYwWlhkaGVTNWpiMjBpZlEuZXlKbGVIQWlPakUzTlRFMU9EQTROemdzSW1wMGFTSTZJbU16TURSaE5EY3pMV013WlRFdE5ESXhOUzA0TnpBNUxXRTJOamd3TldReVpqWXdZeUlzSW5OMVlpSTZJamg2ZG0wNGVuYzFlWGsxTkdwemNYWWlMQ0pwYzNNaU9pSm9kSFJ3Y3pvdkwyRndhUzV6WVc1a1ltOTRMbUp5WVdsdWRISmxaV2RoZEdWM1lYa3VZMjl0SWl3aWJXVnlZMmhoYm5RaU9uc2ljSFZpYkdsalgybGtJam9pT0hwMmJUaDZkelY1ZVRVMGFuTnhkaUlzSW5abGNtbG1lVjlqWVhKa1gySjVYMlJsWm1GMWJIUWlPbVpoYkhObGZTd2ljbWxuYUhSeklqcGJJbTFoYm1GblpWOTJZWFZzZENKZExDSnpZMjl3WlNJNld5SkNjbUZwYm5SeVpXVTZWbUYxYkhRaVhTd2liM0IwYVc5dWN5STZlMzE5LnFOdXRBNktFYzVWNFV0TXRPY25CdENKRW9ITzN5N3UzcmYwcmZRcS1Wc0stNU1KWTJITFh6eWF0enNWSEJSUnpjcDB0V2psdlJ0X0YzM3BvWmtodjBnIiwiY29uZmlnVXJsIjoiaHR0cHM6Ly9hcGkuc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbTo0NDMvbWVyY2hhbnRzLzh6dm04enc1eXk1NGpzcXYvY2xpZW50X2FwaS92MS9jb25maWd1cmF0aW9uIiwiZ3JhcGhRTCI6eyJ1cmwiOiJodHRwczovL3BheW1lbnRzLnNhbmRib3guYnJhaW50cmVlLWFwaS5jb20vZ3JhcGhxbCIsImRhdGUiOiIyMDE4LTA1LTA4IiwiZmVhdHVyZXMiOlsidG9rZW5pemVfY3JlZGl0X2NhcmRzIl19LCJjbGllbnRBcGlVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvOHp2bTh6dzV5eTU0anNxdi9jbGllbnRfYXBpIiwiZW52aXJvbm1lbnQiOiJzYW5kYm94IiwibWVyY2hhbnRJZCI6Ijh6dm04enc1eXk1NGpzcXYiLCJhc3NldHNVcmwiOiJodHRwczovL2Fzc2V0cy5icmFpbnRyZWVnYXRld2F5LmNvbSIsImF1dGhVcmwiOiJodHRwczovL2F1dGgudmVubW8uc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbSIsInZlbm1vIjoib2ZmIiwiY2hhbGxlbmdlcyI6W10sInRocmVlRFNlY3VyZUVuYWJsZWQiOnRydWUsImFuYWx5dGljcyI6eyJ1cmwiOiJodHRwczovL29yaWdpbi1hbmFseXRpY3Mtc2FuZC5zYW5kYm94LmJyYWludHJlZS1hcGkuY29tLzh6dm04enc1eXk1NGpzcXYifSwicGF5cGFsRW5hYmxlZCI6dHJ1ZSwicGF5cGFsIjp7ImJpbGxpbmdBZ3JlZW1lbnRzRW5hYmxlZCI6dHJ1ZSwiZW52aXJvbm1lbnROb05ldHdvcmsiOnRydWUsInVudmV0dGVkTWVyY2hhbnQiOmZhbHNlLCJhbGxvd0h0dHAiOnRydWUsImRpc3BsYXlOYW1lIjoiU2hlbGwiLCJjbGllbnRJZCI6bnVsbCwiYmFzZVVybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXNzZXRzVXJsIjoiaHR0cHM6Ly9jaGVja291dC5wYXlwYWwuY29tIiwiZGlyZWN0QmFzZVVybCI6bnVsbCwiZW52aXJvbm1lbnQiOiJvZmZsaW5lIiwiYnJhaW50cmVlQ2xpZW50SWQiOiJtYXN0ZXJjbGllbnQzIiwibWVyY2hhbnRBY2NvdW50SWQiOiJzaGVsbCIsImN1cnJlbmN5SXNvQ29kZSI6IkVVUiJ9fQ=='
+            // localStorage.setItem('prevClientToken', hardcoded)
+            // localStorage.setItem('clientToken', hardcoded)
+
             resolve(true)
           } else {
             localStorage.removeItem('clientToken')
