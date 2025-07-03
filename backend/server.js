2da30e0d7a18d2fe99e4f8233740131cd9fd384b
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-07-03 12:39:43 +0100

    hunk

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,21 @@ app.get('/client-token', (req, res) => {
   })
 })
 
+app.post('/transaction/sale', (req, res) => {
+  const amount = req.body.amount
+  const nonce = req.body.nonce
+  const deviceData = req.body.deviceData
+
+  gateway.transaction.sale({
+    amount,
+    paymentMethodNonce: nonce,
+    deviceData,
+    options: { submitForSettlement: true },
+  }).then(result => {
+    res.json(result)
+  })
+})
+
 app.listen(port, () => {
   console.log(`braintree-be listening on port ${port}`)
 })
