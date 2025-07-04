9cd817a0284e8ead0e9219ad7009b1271f3b36b5
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-07-04 16:19:36 +0100

    hunk

---
+++ frontend/src/app/routes/route3/components/route3/route3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-route3',
+  standalone: true,
+  imports: [],
+  templateUrl: './route3.component.html',
+  styleUrl: './route3.component.scss',
+})
+export class Route3Component {}
