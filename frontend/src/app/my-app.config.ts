c5702266ce7ff4cc7dc9d60415c762b6b86ac533
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-07-04 17:21:04 +0100

    braintree finished

--- frontend/src/app/my-app.config.ts
+++ frontend/src/app/my-app.config.ts
@@ -2,5 +2,5 @@ import { IMyAppConfig } from '@interfaces/i-my-app-config'
 
 export const myAppConfig: IMyAppConfig = {
   email: 'test@example.com',
-  transactionsUrl: 'https://sandbox.braintreegateway.com/merchants/dvk7crxxggftjq3p/transactions/advanced_search',
+  transactionsUrl: 'https://sandbox.braintreegateway.com',
 }
