a2786abf99c30f34c8e0a2d2259c5c7f1a4aa329
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-07-04 13:58:47 +0100

    hunk

--- frontend/src/app/routes/route2/components/route2/route2.component.ts
+++ frontend/src/app/routes/route2/components/route2/route2.component.ts
@@ -1,11 +1,15 @@
-import { Component, inject, OnInit, signal } from '@angular/core'
-import braintree from 'braintree-web'
+import { Component, computed, effect, ElementRef, inject, OnInit, signal, viewChild } from '@angular/core'
+import braintree, { Client, DataCollector, HostedFields, HostedFieldsTokenizePayload } from 'braintree-web'
 import { switchMap } from 'rxjs'
 import { ICustomer } from '@interfaces/i-customer'
 import { IClientToken } from '@interfaces/i-client-token'
 import { HttpErrorResponse } from '@angular/common/http'
 import { BraintreeService } from '@services/braintree.service'
 import { myAppConfig } from '../../../../my-app.config'
+import { IError } from '@interfaces/i-error'
+import { ToastrService } from 'ngx-toastr'
+import { IBraintreeTransactionSalePayload } from '@interfaces/i-braintree-transaction-sale-payload'
+import { IBraintreeTransactionSaleResponse } from '@interfaces/i-braintree-transaction-sale-response'
 
 @Component({
   selector: 'app-route2',
