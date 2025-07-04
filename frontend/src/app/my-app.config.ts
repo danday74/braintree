638a289c4b43052902bfde6325a908fe1b8c205f
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-07-04 15:55:29 +0100

    hunk

--- frontend/src/app/my-app.config.ts
+++ frontend/src/app/my-app.config.ts
@@ -2,4 +2,5 @@ import { IMyAppConfig } from '@interfaces/i-my-app-config'
 
 export const myAppConfig: IMyAppConfig = {
   email: 'test@example.com',
+  transactionsUrl: 'https://sandbox.braintreegateway.com/merchants/dvk7crxxggftjq3p/transactions/advanced_search',
 }
