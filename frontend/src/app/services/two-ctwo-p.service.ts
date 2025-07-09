0fc6ed1af384cad405c7d48655b47621994e970b
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-07-09 17:55:46 +0100

    BE commit

--- frontend/src/app/services/two-ctwo-p.service.ts
+++ frontend/src/app/services/two-ctwo-p.service.ts
@@ -2,13 +2,14 @@ import { inject, Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { I2c2pPayload } from '@interfaces/i-2c2p-payload'
 import { Observable } from 'rxjs'
+import { I2c2pResponse } from '@interfaces/i-2c2p-response'
 
 @Injectable({ providedIn: 'root' })
 export class TwoCTwoPService {
   private readonly http: HttpClient = inject(HttpClient)
 
-  processPayment(payload: I2c2pPayload): Observable<unknown> {
+  processPayment(payload: I2c2pPayload): Observable<I2c2pResponse> {
     const url = '/api/2c2p/process-payment'
-    return this.http.post<unknown>(url, payload) // .pipe(delay(2000))
+    return this.http.post<I2c2pResponse>(url, payload)
   }
 }
