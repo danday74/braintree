7f610bc6283d47b485a1dd2513602b268d7a7825
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-07-09 16:45:25 +0100

    hunk

--- frontend/src/app/my-app.config.ts
+++ frontend/src/app/my-app.config.ts
@@ -2,6 +2,5 @@ import { IMyAppConfig } from '@interfaces/i-my-app-config'
 
 export const myAppConfig: IMyAppConfig = {
   email: 'test@example.com',
-  transactionsUrl: 'https://sandbox.braintreegateway.com',
   defaultCurrency: 'GBP',
 }
