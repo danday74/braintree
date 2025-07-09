4c868216f25c7c96f64f08d9d1f067c404315d12
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-07-09 23:48:40 +0100

    BE commit

---
+++ backend/js/do-2c2p-payment.js
@@ -0,0 +1,33 @@
+const axios = require('axios')
+
+// see https://developer.2c2p.com/docs/api-do-payment
+const do2c2pPayment = async (paymentToken, encryptedCardInfo) => {
+
+  const url = 'https://sandbox-pgw.2c2p.com/payment/4.3/payment'
+  const payload = {
+    paymentToken,
+    payment: {
+      code: {
+        channelCode: 'CC', // Credit Card
+      },
+      data: {
+        securePayToken: encryptedCardInfo,
+      },
+    },
+  }
+  const headers = { 'Content-Type': 'application/json' }
+  const response = await axios.post(url, payload, { headers })
+  const responseData = response.data
+
+  return {
+    success: responseData.respCode === '2000',
+    responseData,
+    ...responseData,
+    invoiceNo: responseData.invoiceNo ?? null,
+    channelCode: responseData.channelCode ?? null,
+    respCode: responseData.respCode,
+    respDesc: responseData.respDesc,
+  }
+}
+
+module.exports = do2c2pPayment
