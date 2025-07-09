c1d39d34e8ebb35fc654aa2b0fc863cd06b86368
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-07-09 16:31:29 +0100

    hunk

--- frontend/src/app/interfaces/i-2c2p-payload.ts
+++ frontend/src/app/interfaces/i-2c2p-payload.ts
@@ -1,6 +1,5 @@
 export interface I2c2pPayload {
-  cardNumber: string
-  expiryMonth: string
-  expiryYear: string
-  cvv: string
+  encryptedCardInfo: string
+  amount: number
+  currency: string
 }
